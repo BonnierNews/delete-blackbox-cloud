d864aa301987195afb85c514e4979cd6fd6e65e8
Sara Linnea Berg <sara.linnea.berg@bonniernews.se>
2020-03-04 11:17:40 +0100

    Removed terraform resources

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -2,7 +2,7 @@ data "aws_ami" "blackbox_ami" {
   provider   = "aws.selected_region"
   most_recent = true
   owners = ["self"]
-  
+
   filter {
     name   = "name"
     values = ["blackbox-cloud/${var.blackbox_version}/*"]
@@ -15,31 +15,5 @@ data "template_file" "cloud_init" {
     fqdn          = "${local.instance_fqdn}"
     password_hash = "${var.blackbox_password_hash}"
     username      = "${var.os_username}"
-  }
-}
-
-resource "aws_key_pair" "key_pair" {
-  provider   = "aws.selected_region"
-  key_name   = "blackbox"
-  public_key = "${var.ssh_public_key}"
-}
-
-resource "aws_instance" "box" {
-  provider      = "aws.selected_region"
-  ami           = "${data.aws_ami.blackbox_ami.id}"
-  instance_type = "${var.instance_type}"
-  user_data     = "${data.template_file.cloud_init.rendered}"
-  key_name      = "${aws_key_pair.key_pair.key_name}"
-
-  vpc_security_group_ids      = ["${aws_security_group.sec_group.id}"]
-  subnet_id                   = "${aws_subnet.blackbox_subnet.id}"
-  associate_public_ip_address = true
-
-  root_block_device {
-    delete_on_termination = true
-  }
-
-  tags {
-    module = "blackbox-cloud"
   }
 }
